ff6491633a320d557624195d2601a385eefdc803
Denis-G25 <gabordenis1@gmail.com>
2025-03-27 21:23:46 +0200

    Update Patrat.java

--- Patrat.java
+++ Patrat.java
@@ -1,28 +1,28 @@
-// Patrat.java
+
 public class Patrat {
     private String latura;
 
-    // Constructor fără parametri (latura = 10)
+   
     public Patrat() {
         this.latura = "10";
     }
 
-    // Constructor cu parametru
+    
     public Patrat(String latura) {
         this.latura = latura;
     }
 
-    // Getter pentru latura
+   
     public String getLatura() {
         return latura;
     }
 
-    // Setter pentru latura
+   
     public void setLatura(String latura) {
         this.latura = latura;
     }
 
-    // Metodă pentru calcularea ariei
+   
     public String calculeazaAria() {
         return toString;
     }
